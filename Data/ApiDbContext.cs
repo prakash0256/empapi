92858c920f9075f9d22f901669518bef828efdb7
prakash0256 <prakashkancharla2002@gmail.com>
2023-10-01 19:11:56 +0530

    EmployeApi implemented

---
+++ Data/ApiDbContext.cs
@@ -0,0 +1,11 @@
+﻿using EmployeeApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeApi.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options){ }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeApi.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public int EmpId { get; set; }
+        public string ? EmpName { get; set; }
+        public string? Skills { get; set; }
+        public string? MobileNumber { get; set; }
+    }
+}
